127045d7c12834a8c725f8903d5d88a6ef359107
Gebruiker2 <Gebruiker2@DESKTOP-GLCR0C1>
2026-08-24 09:39:05 +0200

    Add project files.

---
+++ DatingApp/Controllers/MemberController.cs
@@ -0,0 +1,30 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController(AppDbContext context) : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<AppUser>>> GetAllUsers()
+        {
+            var members = await context.Users.ToListAsync();
+
+            return members;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserById(int id)
+        {
+            var member = await context.Users.FindAsync(id);
+            if ( member != null)
+                return member;
+            else
+                return NotFound();
+        }
+    }
+}
---
+++ DatingApp/Program.cs
@@ -0,0 +1,54 @@
+using DatingApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyCors", builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string not found");
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(config =>
+                {
+                    config.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+                    config.RoutePrefix = "";
+                });
+            }
+            app.UseCors("MyCors");
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
