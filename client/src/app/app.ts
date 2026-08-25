d7468e4a9876151a9c0e68d75841de04b31950aa
Mathub <Gebruiker2@DESKTOP-GLCR0C1>
2026-08-25 15:09:55 +0200

    end of day

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,26 +3,28 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  imports: [],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+    selector: 'app-root',
+    imports: [],
+    templateUrl: './app.html',
+    styleUrl: './app.css',
 })
 export class App implements OnInit {
-  private http = inject(HttpClient);
-  protected readonly title = signal('Dating app');
-  protected members = signal<any>([]);
+    private http = inject(HttpClient);
+    protected readonly title = signal('Dating app');
+    protected members = signal<any>([]);
 
-  async ngOnInit() {
-    this.members.set(await this.getMembers());
-  }
-
-  async getMembers() {
-    try {
-      return lastValueFrom(this.http.get('https://localhost:7216/api/members'));
-    } catch (error) {
-      console.log(error);
-      throw error;
+    async ngOnInit() {
+        this.members.set(await this.getMembers());
     }
-  }
+
+    async getMembers() {
+        try {
+            return lastValueFrom(
+                this.http.get('https://localhost:7216/api/members'),
+            );
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
 }
