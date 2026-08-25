66b54c1339ed42081c89f52c2c23590781c8abff
Mathub <Gebruiker2@DESKTOP-GLCR0C1>
2026-08-25 11:53:42 +0200

    add tailwind and daisyui

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,18 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 export class App implements OnInit{
   private http = inject(HttpClient);
   protected readonly title = signal('Dating app');
-  protected members = signal<any>(null)
+  protected members = signal<any>([])
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:7216/api/members').subscribe({
-      next: Response => this.members.set(Response),
-      error: err => console.log(err),
-      complete: () => console.log('Completed the request.')
-    })
+  async ngOnInit() {
+    this.members.set(await this.getMembers())
+  }
+
+  async getMembers() {
+    try{
+      return lastValueFrom(this.http.get('https://localhost:7216/api/members'));
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 }
