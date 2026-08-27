d65fed2c4c546fe04d863e888961fc40b63669a8
Mathub <Gebruiker2@DESKTOP-GLCR0C1>
2026-08-27 12:25:56 +0200

    add basic user registry and login

--- DatingApp/Controllers/AccountController.cs
+++ DatingApp/Controllers/AccountController.cs
@@ -1,29 +1,55 @@
 ﻿using DatingApp.Data;
+using DatingApp.DTOs;
 using DatingApp.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace DatingApp.Controllers
 {
-    public class AccountController(AppDbContext dbContext) : BaseApiController
+    public class AccountController(AppDbContext context) : BaseApiController
     {
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string email, string displayName, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
-            using var hmac = new HMACSHA512();
+            if (await EmailExists(registerDto.Email)) return BadRequest("Email taken.");
+
+            var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                DisplayName = displayName,
-                Email = email,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
 
-            await dbContext.Users.AddAsync(user);
-            await dbContext.SaveChangesAsync();
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
 
             return user;
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email);
+
+            if(user == null) return Unauthorized("Invalid email address.");
+
+            var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (var i = 0; i < computedHash.Length; i++)
+                if (computedHash[i] != user.PasswordHash[i]) 
+                    return Unauthorized("Invalid Password.");
+
+            return user;
+        }
+
+        private async Task<bool> EmailExists(string email)
+        {
+            return await context.Users.AnyAsync(x => x.Email.ToLower().Equals(email.ToLower()));
         }
     }
 }
