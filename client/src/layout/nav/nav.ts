4702e74e0fb87e207f94fd6eabce3301b88a81b8
Mathub <Gebruiker2@DESKTOP-GLCR0C1>
2026-09-02 14:57:20 +0200

    add folder make up and nav component

---
+++ client/src/layout/nav/nav.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    imports: [],
+    selector: 'app-nav',
+    styleUrl: './nav.css',
+    templateUrl: './nav.html',
+})
+export class Nav {}
