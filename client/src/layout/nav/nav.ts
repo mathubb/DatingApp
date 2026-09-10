5e4790c7aacce52ca1aedc8a5962c00aae8216e5
Mathub <Gebruiker2@DESKTOP-GLCR0C1>
2026-09-10 14:24:52 +0200

    add img

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -9,14 +9,22 @@ import { AccountService } from '../../core/services/account-service';
     templateUrl: './nav.html',
 })
 export class Nav {
-    private account = inject(AccountService)
-    protected creds: any = {}
-    protected loggedIn = signal(false)
+    private account = inject(AccountService);
+    protected creds: any = {};
+    protected loggedIn = signal(false);
 
     login() {
         this.account.login(this.creds).subscribe({
-            next: result => console.log(result),
-            error: err => alert(err)
-        })
+            next: (result) => {
+                console.log(result);
+                this.loggedIn.set(true);
+                this.creds = {};
+            },
+            error: (err) => alert(err),
+        });
+    }
+
+    logout() {
+        this.loggedIn.set(false);
     }
 }
